bdd0f806770fddde44cbadb5e502ab0082f61a59
Javad <ja7ad@live.com>
2022-08-11 11:26:07 +0430

    add: get random port of available from kernel

--- worker/driver.go
+++ worker/driver.go
@@ -4,25 +4,31 @@ import (
 	"context"
 	"fmt"
 	"github.com/tebeka/selenium"
+	"net"
 	"sync"
 )
 
 type result struct {
 	target string
+	proxy  string
 	title  string
 	err    error
 }
 
-func startDriver(ctx context.Context, target string, proxy string, seleniumServerPath, driverPath string, port int, res chan result, wg *sync.WaitGroup) {
+func startDriver(ctx context.Context, target string, proxy string, seleniumServerPath, driverPath string, res chan result, wg *sync.WaitGroup) {
 	defer wg.Done()
 	opts := []selenium.ServiceOption{
 		selenium.StartFrameBuffer(),
 		selenium.ChromeDriver(driverPath),
 		selenium.Output(nil),
 	}
+	port, err := getFreePort()
+	if err != nil {
+		res <- result{target, proxy, "", err}
+	}
 	service, err := selenium.NewSeleniumService(seleniumServerPath, port, opts...)
 	if err != nil {
-		res <- result{target, "", err}
+		res <- result{target, proxy, "", err}
 	}
 	defer service.Stop()
 	caps := selenium.Capabilities{"browserName": "chrome"}
@@ -32,21 +38,34 @@ func startDriver(ctx context.Context, target string, proxy string, seleniumServe
 	})
 	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
 	if err != nil {
-		res <- result{target, "", err}
+		res <- result{target, proxy, "", err}
 	}
 	defer wd.Quit()
 	if err := wd.Get(target); err != nil {
-		res <- result{target, "", err}
+		res <- result{target, proxy, "", err}
 	}
 	title, err := wd.Title()
-	res <- result{target, title, err}
+	res <- result{target, proxy, title, err}
 
 	for {
 		select {
 		case <-ctx.Done():
-			res <- result{target, "", ctx.Err()}
+			res <- result{target, proxy, "", ctx.Err()}
 		case <-res:
 		}
 	}
+}
 
+func getFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
--- worker/driver_test.go
+++ worker/driver_test.go
@@ -11,28 +11,27 @@ func TestStartDriver(t *testing.T) {
 	seleniumPath := "/home/user/Project/go/Personal/selenium/download/selenium-server.jar"
 	chromePath := "/home/user/Project/go/Personal/selenium/download/chromedriver"
 	tests := []struct {
-		Target     string
-		Proxy      string
-		DriverPort int
+		Target string
+		Proxy  string
 	}{
-		{"https://google.com", "41.65.236.44:1976", 8080},
-		{"https://bing.com", "41.65.236.44:1976", 8081},
-		{"https://soft98.ir", "41.65.236.44:1976", 8082},
-		{"https://bscscan.com/token/0xacfc95585d80ab62f67a14c566c1b7a49fe91167", "41.65.236.44:1976", 8083},
-		{"https://www.coingecko.com/en/coins/feg-token-bsc", "41.65.236.44:1976", 8084},
+		{"https://google.com", "41.65.236.44:1976"},
+		{"https://bing.com", "41.65.236.44:1976"},
+		{"https://soft98.ir", "41.65.236.44:1976"},
+		{"https://bscscan.com/token/0xacfc95585d80ab62f67a14c566c1b7a49fe91167", "41.65.236.44:1976"},
+		{"https://www.coingecko.com/en/coins/feg-token-bsc", "41.65.236.44:1976"},
 	}
 	res := make(chan result)
 	wg := &sync.WaitGroup{}
 	wg.Add(len(tests))
 	for _, tt := range tests {
 		ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
-		go startDriver(ctx, tt.Target, tt.Proxy, seleniumPath, chromePath, tt.DriverPort, res, wg)
+		go startDriver(ctx, tt.Target, tt.Proxy, seleniumPath, chromePath, res, wg)
 	}
 	for result := range res {
 		if result.err != nil {
 			t.Fatalf("target %v get error %v", result.target, result.err)
 		}
-		t.Logf("target %v title %v viewed", result.target, result.title)
+		t.Logf("target %v title %v viewed with proxy %v", result.target, result.title, result.proxy)
 	}
 	close(res)
 	wg.Wait()
